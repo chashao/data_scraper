c172a98ec2daf9a878d439a3006ce775f35e2d57
chashao <melvin94@gmail.com>
2016-06-30 22:19:04 -0700

    more documentation and server connection code improvement

--- extractor.py
+++ extractor.py
@@ -1,12 +1,14 @@
-#Connects to the database specified and pulls the datatable requested, writing it to a csv file
-
+# Connects to the database specified and pulls the datatable requested, writing it to a csv file
+# MS SQL Server 2012 and 2014 uses Native Client 11.0. Change the other elements of the string to 
+# connect to the desired server.
 
 import csv
 from import_script import package_installer
 
 package_installer('pyodbc')
-connection = pyodbc.connect('server to connect later')
+pyodbc.connect(r'DRIVER={SQL Server Native Client 11.0};SERVER = test;DATABASE = test;UID = user;PWD = password')
 cursor = connection.cursor()
+
 cursor.execute("SQL query to extract the necessary tables from the database")
 
 
