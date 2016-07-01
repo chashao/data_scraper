45b8f55f99f9f9a9bbe1cec1e8d80a75142713b8
chashao <melvin94@gmail.com>
2016-06-30 21:49:03 -0700

    pulling csv from database

--- parser.py
+++ parser.py
@@ -1,4 +1,13 @@
 import csv
-import pyodbc
+from import_script import package_installer
 
-connection = pyodbc.connect('Driver = (SQL Server);', )
+package_installer('pyodbc')
+connection = pyodbc.connect('server to connect later')
+cursor = connection.cursor()
+cursor.execute("SQL query to extract the necessary tables from the database")
+
+
+with open("datatable.csv", "w+") as datatable:
+	writer = csv.writer(datatable)
+	for row in cursor.fetchall():
+		writer.writerow(row)
