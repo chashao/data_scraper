d25b76a347531ae8da02f14e66bdde415461bceb
chashao <melvin.huang@berkeley.edu>
2016-07-14 16:04:47 -0700

    again, the algorithm works, now again, to make it look pretty and to clean up all the extra files I have lying around

--- extractor.py
+++ extractor.py
@@ -54,32 +54,17 @@ val_file_path = r"C:\Users\melvin.huang\Desktop\validation.csv"
 with open(new_file_path, "rb") as validation_file:
 	with open(val_file_path, "w+") as formatted_file:
 		validation_file_read = csv.reader(validation_file)
-		formatted_file_write = csv.writer(formatted_file)
+		formatted_file_write = csv.writer(formatted_file, delimiter = ",")
 		validation_file_read.next()
+
 		for row in validation_file_read:
-			formatted_file_write.writerow([
-				row[0],
-				row[2], 
-				row[3], 
-				row[4],
-				row[5],
-				row[6],
-				row[7],
-				row[8],
-				row[9],
-				row[10],
-				row[11],
-				row[12],
-				row[13],
-				row[14],
-				row[15],
-				row[16],
-				row[17],
-				row[18],
-				row[19]])
+			index = 2
+			while index <= (len(row) - 2):
+				formatted_file_write.writerow([row[0], row[index], row[index + 1]])
+				index += 2
 
 os.remove(new_file_path)
-
+print "checkpoint 0"
 
 #paths to each file
 trays_path = r"C:\Users\melvin.huang\Desktop\trays.csv"
@@ -92,7 +77,9 @@ with open(trays_path, "w+") as trayfile:
 	cursor.execute("SELECT TrayID FROM [41_53116].[dbo].[tray] ORDER BY adddt DESC")
 	writer = csv.writer(trayfile)
 	for row in cursor.fetchall():
+	
 		writer.writerow(row)
+print "checkpoint 1"
 
 #for each tray ID, output the wells and samples
 with open(trays_path, "rb") as trayfile:
@@ -100,58 +87,43 @@ with open(trays_path, "rb") as trayfile:
 		well_writer = csv.writer(wellsfile)
 		tray_reader = csv.reader(trayfile)
 		for entry in tray_reader:
-			print entry
 			cursor.execute("SELECT WellID, SampleIDName FROM [41_53116].[dbo].[well],[41_53116].[dbo].[sample] WHERE TrayID = ? AND [well].SampleID = [sample].SampleID", entry)
 			for line in cursor.fetchall():
 				well_writer.writerow(line)
 
-
+print "checkpoint 2"
 
 with open(wells_path, "rb") as wells_file:
 	with open(alleles_path, "w+") as allele_file:
 		wells_reader = csv.reader(wells_file)
-		writer = csv.writer(allele_file, quoting = csv.QUOTE_MINIMAL)
+		writer = csv.writer(allele_file, delimiter = " ", quoting = csv.QUOTE_MINIMAL)
 		for line in wells_reader:
 			well_id = line[0]
 			sample_id_name = line[1]
 			cursor.execute("SELECT Value01 FROM [41_53116].[dbo].[WELL_RESULT] WHERE ResultType = '01' AND wellid =?", well_id)
 			for line in cursor.fetchall():
 				stringify = "".join(line)
-				writer.writerow([sample_id_name, stringify])
+				first_allele = stringify.split()[0]
+				second_allele = stringify.split()[1]
+				writer.writerow([sample_id_name, first_allele, second_allele])
 
-"""
+print "checkpoint 3"
+
+
 with open(val_file_path, "rb") as val_file:
+	val_reader = csv.reader(val_file)
 	with open(alleles_path, "rb") as allele_file:
-		val_reader = csv.reader(val_file)
-		allele_reader = csv.reader(alleles_file)
+		read_to_str = mmap.mmap(allele_file.fileno(), 0, access = mmap.ACCESS_READ)
 		for row in val_reader:
-			for line in allele_reader:
-				if 
-				
+			stringify = " ".join(row)
+			if read_to_str.find(stringify) != -1:
+				print row[0], "found match on pair", row[1], row[2]
+			#else:
+			#	print row[0], "did not find match on pair", row[1], row[2]
 
-"""
+
 			#analysis(formatted_file, sample_id_name)
 
 
 		#insert function to read the file and do analysis to it.
 
-"""
-with open(r"C:\Users\melvin.huang\Desktop\validation.csv", "rb") as formatted_file:
-	with open("C:\Users\melvin.huang\Desktop\datatable.csv", "rb") as datatable:
-#file-splitting operation:
-		formatted_reader = csv.reader(formatted_file)
-		read_to_str = mmap.mmap(datatable.fileno(), 0, access = mmap.ACCESS_READ)
-		for row in formatted_reader:
-			index = 0
-			num_matches = 0
-			while index <= (len(row) - 2):
-				chunk = [row[index], row[index + 1]]
-				chunk_string = " ".join(chunk)
-				if read_to_str.find(chunk_string) != -1:
-					print(chunk_string)
-					index += 2
-				else:
-					index += 2
-					print "Can't find this"
-
-"""
