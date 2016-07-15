b6072f0cb5ddb75138ffb4f7037a4f08c958a88b
chashao <melvin.huang@berkeley.edu>
2016-07-15 10:41:11 -0700

    OneRepublic

--- extractor.py
+++ extractor.py
@@ -3,8 +3,6 @@ import csv
 import os
 import shutil
 import mmap
-import io
-
 
 
 # run for windows machines to install the necessary packages to run the rest of the code
@@ -27,6 +25,7 @@ path_to_validation_file = r"C:\Users\melvin.huang\Desktop\06-19-14 NGS Validatio
 filename, file_ext = os.path.splitext(path_to_validation_file)
 new_file_path = filename + '.csv'
 
+#turns the xls file to a csv for ease of use
 def csv_from_excel(excel_file, csv_file):
     wb = xlrd.open_workbook(excel_file)
     sheet = wb.sheet_by_index(0)
@@ -40,17 +39,33 @@ def csv_from_excel(excel_file, csv_file):
 
 csv_from_excel(path_to_validation_file, new_file_path)
 
+print "Lately, I've been, I've been losing sleep"
+
 # Connects to the database specified and pulls the datatable requested, writing it to a csv file
 # MS SQL Server 2012 and 2014 uses Native Client 11.0. Change the other elements of the string to 
 # connect to the desired server.
 
 # DO NOT TOUCH THE FORMATTING OF THE LINE BELOW!!!!!!
+#will eventually variable-ize these...
 connection = pyodbc.connect(r'DRIVER={SQL Server Native Client 11.0};' r'SERVER=(local)\FUSION_SQL14EXP;' \
 	r'DATABASE=41_53116;' r'TRUSTED_CONNECTION=yes;')
 cursor = connection.cursor()
 
+#paths to each file
 val_file_path = r"C:\Users\melvin.huang\Desktop\validation.csv"
 
+trays_path = r"C:\Users\melvin.huang\Desktop\trays.csv"
+wells_path = r"C:\Users\melvin.huang\Desktop\wells.csv"
+alleles_path = r"C:\Users\melvin.huang\Desktop\alleles.csv"
+results_path = r"C:\Users\melvin.huang\Desktop\results.csv"
+
+
+#SQL queries - will eventually figure out how to turn the database name into a variable
+tray_selector = "SELECT TrayID FROM [41_53116].[dbo].[tray] ORDER BY adddt DESC"
+well_and_sample_selector = "SELECT WellID, SampleIDName FROM [41_53116].[dbo].[well],[41_53116].[dbo].[sample] WHERE TrayID = ? AND [well].SampleID = [sample].SampleID"
+allele_selector = "SELECT Value01 FROM [41_53116].[dbo].[WELL_RESULT] WHERE ResultType = '01' AND wellid =?"
+
+#modifies the validation file into something that can be easily parsed
 with open(new_file_path, "rb") as validation_file:
 	with open(val_file_path, "w+") as formatted_file:
 		validation_file_read = csv.reader(validation_file)
@@ -64,22 +79,17 @@ with open(new_file_path, "rb") as validation_file:
 				index += 2
 
 os.remove(new_file_path)
-print "checkpoint 0"
 
+print "Dreaming about the things that we could be"
 #paths to each file
-trays_path = r"C:\Users\melvin.huang\Desktop\trays.csv"
-wells_path = r"C:\Users\melvin.huang\Desktop\wells.csv"
-alleles_path = r"C:\Users\melvin.huang\Desktop\alleles.csv"
-
 
 #Returns a csv of all the tray IDs
 with open(trays_path, "w+") as trayfile:
-	cursor.execute("SELECT TrayID FROM [41_53116].[dbo].[tray] ORDER BY adddt DESC")
+	cursor.execute(tray_selector)
 	writer = csv.writer(trayfile)
 	for row in cursor.fetchall():
-	
 		writer.writerow(row)
-print "checkpoint 1"
+print "But baby, I've been, I've been praying hard"
 
 #for each tray ID, output the wells and samples
 with open(trays_path, "rb") as trayfile:
@@ -87,12 +97,12 @@ with open(trays_path, "rb") as trayfile:
 		well_writer = csv.writer(wellsfile)
 		tray_reader = csv.reader(trayfile)
 		for entry in tray_reader:
-			cursor.execute("SELECT WellID, SampleIDName FROM [41_53116].[dbo].[well],[41_53116].[dbo].[sample] WHERE TrayID = ? AND [well].SampleID = [sample].SampleID", entry)
+			cursor.execute(well_and_sample_selector, entry)
 			for line in cursor.fetchall():
 				well_writer.writerow(line)
 
-print "checkpoint 2"
-
+print "Said no more counting dollars"
+#tags each allele pair with the corresponding sample ID name
 with open(wells_path, "rb") as wells_file:
 	with open(alleles_path, "w+") as allele_file:
 		wells_reader = csv.reader(wells_file)
@@ -100,17 +110,16 @@ with open(wells_path, "rb") as wells_file:
 		for line in wells_reader:
 			well_id = line[0]
 			sample_id_name = line[1]
-			cursor.execute("SELECT Value01 FROM [41_53116].[dbo].[WELL_RESULT] WHERE ResultType = '01' AND wellid =?", well_id)
+			cursor.execute(allele_selector, well_id)
 			for line in cursor.fetchall():
 				stringify = "".join(line)
 				first_allele = stringify.split()[0]
 				second_allele = stringify.split()[1]
 				writer.writerow([sample_id_name, first_allele, second_allele])
 
-print "checkpoint 3"
+print "We'll be counting stars"
 
-results_path = r"C:\Users\melvin.huang\Desktop\results.csv"
-
+#comparison algorithm
 with open(val_file_path, "rb") as val_file:
 	val_reader = csv.reader(val_file)
 	with open(alleles_path, "rb") as allele_file:
@@ -125,7 +134,7 @@ with open(val_file_path, "rb") as val_file:
 				else:
 					results_writer.writerow([row[0], row[1], row[2], "NO"])
 
-			#analysis(formatted_file, sample_id_name)
-
-
-		#insert function to read the file and do analysis to it.
+print "Yeah we'll be counting stars"
+os.remove(trays_path)
+os.remove(wells_path)
+os.remove(val_file_path)
